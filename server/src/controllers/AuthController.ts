c73cbe649df6d6b2c6547fb15b93d6d697de3f56
João Conte <lejoaoconte@gmail.com>
2022-07-17 14:02:04 -0300

    Finish backend part

---
+++ server/src/controllers/AuthController.ts
@@ -0,0 +1,64 @@
+import { Request, Response, NextFunction } from "express";
+import "dotenv/config";
+
+import jwt from "jsonwebtoken";
+import User from "../schemas/User";
+
+export interface AuthControllerInterface extends Document {
+  name: string;
+  github: string;
+  role: string;
+}
+
+class AuthController {
+  public authorization(req: Request, res: Response, next: NextFunction) {
+    const token =
+      req?.body?.token ||
+      req?.query?.token ||
+      req?.params?.token ||
+      req.headers["x-access-token"];
+
+    if (!token) {
+      res.status(401).json({
+        message: "Access denied",
+      });
+    } else {
+      jwt.verify(
+        token,
+        process.env.SALT_KEY || "",
+        function (error: any, decode: any) {
+          if (error) {
+            res.status(401).json({
+              message: "Invalid token",
+            });
+          } else {
+            next();
+          }
+        }
+      );
+    }
+  }
+
+  public async authentication(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const user = await User.find({ email, password });
+
+    if (user.length > 0) {
+      const { name, github, role } = user[0];
+      const token = jwt.sign(
+        { name, github, role } as AuthControllerInterface,
+        process.env.SALT_KEY || "",
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      return res.status(200).send({ name, github, role, token });
+    } else {
+      return res.status(401).send("User not found");
+    }
+  }
+}
+
+export default new AuthController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,15 +2,18 @@ import { Router } from "express";
 
 import UserController from "./controllers/UserController";
 import PostController from "./controllers/PostController";
+import AuthController from "./controllers/AuthController";
 
 const routes = Router();
 
-routes.get("/users", UserController.index);
-routes.get("/users/:id", UserController.findOne);
-routes.post("/users", UserController.create);
+routes.get("/users", AuthController.authorization, UserController.index);
+routes.get("/users/:id", AuthController.authorization, UserController.findOne);
+routes.post("/users", AuthController.authorization, UserController.create);
 
-routes.get("/posts", PostController.index);
-routes.get("/posts/:id", PostController.findOne);
-routes.post("/posts", PostController.create);
+routes.get("/posts", AuthController.authorization, PostController.index);
+routes.get("/posts/:id", AuthController.authorization, PostController.findOne);
+routes.post("/posts", AuthController.authorization, PostController.create);
+
+routes.post("/auth", AuthController.authentication);
 
 export default routes;
